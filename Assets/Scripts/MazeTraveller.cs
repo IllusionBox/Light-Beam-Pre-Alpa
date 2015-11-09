30f24444011aa4ad69f08b9eb4ac722bbcc8ef12
maleenj <maleen.nj@gmail.com>
2015-11-09 10:08:33 +0530

    Pee alpa
    Work done at Oliyas

---
+++ Assets/Scripts/MazeTraveller.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MazeTraveller : MonoBehaviour {
+
+	public List<Waypoint> waypoints = new List<Waypoint>();
+	bool moving = true;
+	Waypoint current;
+	int index = 0;
+	Rigidbody2D rbody;
+
+	// Use this for initialization
+	void Start () {
+		current = waypoints [0];
+		rbody = GetComponent<Rigidbody2D> ();
+		rbody.velocity = (current.transform.position - transform.position) / (current.transform.position - transform.position).magnitude;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (moving) {
+			if ((transform.position - current.transform.position).magnitude < 1 && index < waypoints.Count - 1) {
+				print(waypoints[index].name + " : " + index);
+				index++;
+				current = waypoints[index];
+				rbody.velocity = (current.transform.position - transform.position) / (current.transform.position - transform.position).magnitude;
+			} else if ((transform.position - current.transform.position).magnitude < 1){
+				rbody.velocity = Vector2.zero;
+				moving = false;
+			}
+		}
+	}
+
+}
---
+++ Assets/Scripts/TouchController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchController : MonoBehaviour {
+
+	bool touched = false;
+	private Rigidbody2D rbody;
+	Vector2 start, end;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		rbody = GetComponent<Rigidbody2D> ();
+	}
+
+	void OnMouseDown(){
+		//rbody.velocity = Vector2.zero;
+		//touched = true;
+	}
+
+	void OnMouseUp(){
+		//rbody.velocity = speed * (start - end); 
+		rbody.AddForce (speed*(start - end));
+		//Debug.DrawLine (start, end + new Vector2(transform.position.x, transform.position.y));
+		//transform.Translate (new Vector3 (end.x, end.y, 0));
+		print ("end: "+end +"\tStart: "+ start+"\tMagnitude: "+(start-end).magnitude);
+		touched = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.touches.Length > 0) {
+			/*if(!touched){
+				start = Camera.main.ScreenToWorldPoint(Input.touches[0].position);
+				touched = true;
+			}
+			else{*/
+				end = Camera.main.ScreenToWorldPoint(Input.touches[0].position);
+			//}
+		}
+		if (Input.GetMouseButton(0)) {
+			if(!touched){
+				start = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+				touched = true;
+			}
+			else{
+				end = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			}
+		}
+		if (end != null && start != null) {
+			Debug.DrawLine(start, end);
+		}
+	}
+}
